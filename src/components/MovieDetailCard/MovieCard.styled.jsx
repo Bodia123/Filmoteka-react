553eaeaebf177179daea04ab41a898cfa4fa7f62
Bohdan Zinovkin <bogdan.zinovkin12345@gmail.com>
2023-06-27 14:40:35 +0300

    styled button for add/remove from favorite list

--- src/components/MovieDetailCard/MovieCard.styled.jsx
+++ src/components/MovieDetailCard/MovieCard.styled.jsx
@@ -20,10 +20,22 @@ export const Img = styled.img`
   width: 100%;
 `;
 export const LocalToogle = styled.button`
-  background: none;
   color: #ffffffa1;
+  margin-top: 10px;
+  background: linear-gradient(to bottom, #7892c2 5%, #476e9e 100%);
+  background-color: #7892c2;
+  border-radius: 21px;
+  border: 1px solid #4e6096;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 18px;
+  padding: 12px 30px;
+  text-decoration: none;
+  text-shadow: 9px 0px 13px #283966;
   &:hover,
   &:active {
-    color: orange;
+    box-shadow: 0px 0px 0px 4px #9fb4f2;
   }
 `;
